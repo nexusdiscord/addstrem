6a40f4e46e01d2c16c44298ed6d59379a4a21d8b
nexus <63144643+nexusdiscord@users.noreply.github.com>
2024-07-15 22:46:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-const colors = ['#ff00ff', '#ff9900', '#00ff00', '#ffff00'];
-
-function changeColor() {
-    const bubble = document.querySelector('.bubble');
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    bubble.style.color = randomColor;
-}
-
-setInterval(changeColor, 500);
-
 gsap.to(".bubble", {
     duration: 3,
+    ease: "elastic.out(1, 0.3)",
     y: -20,
     repeat: -1,
-    yoyo: true,
-    ease: "power1.inOut"
+    yoyo: true
 });
