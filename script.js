8450d7e8527b5016610bfd4d6e2ea9dfe1bbdd32
nexus <63144643+nexusdiscord@users.noreply.github.com>
2024-07-15 22:29:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-const bubbleAnimation = () => {
-    const bubbles = document.querySelectorAll('.bubble::before');
-    bubbles.forEach(bubble => {
-        gsap.to(bubble, {
-            duration: 4,
-            y: -100,
-            repeat: -1,
-            yoyo: true,
-            ease: "power1.inOut"
-        });
-    });
-};
-
-bubbleAnimation();
+gsap.to(".bubble::before", {
+    duration: 5,
+    y: -200,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut"
+});
